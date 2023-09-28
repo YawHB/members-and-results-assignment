1ad3ab310e1368d43b6e02431b27364cecedf648
YawHB <yawhboateng@gmail.com>
2023-09-28 19:14:54 +0200

    Deleted unnecessary funktion in the render method

--- listRenderer.js
+++ listRenderer.js
@@ -1,22 +1,18 @@
 import { showMembers } from "./script.js";
 function construct(list, container, itemRenderer) {
-    console.log(list);
     const listRenderer = {
         render() {
             for (const item of list) {
-                insertRow(item);
-            }
-            function insertRow(item) {
                 const html = /*html*/ `
         <tr>
-          <td>${item.name()}</td>
-          <td>${item._active ? "Yes" : "No"}</td>
-          <td>${item._birthday}</td>
-          <td>${item.age()}</td>
-          <td>${item.isSenior() ? "Senior" : "Junior"}</td>
+            <td>${item.name()}</td>
+            <td>${item._active ? "Yes" : "No"}</td>
+            <td>${item._birthday}</td>
+            <td>${item.age()}</td>
+            <td>${item.isSenior() ? "Senior" : "Junior"}</td>
         </tr>
-    
-    `;
+        
+        `;
 
                 document
                     .querySelector(`#${container} tbody`)
